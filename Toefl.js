f9bdcd3321950018d1c2840c2843de074802388b
AlexFanw <alex8802732@gmail.com>
2021-04-12 18:22:22 +0800

    时间比对功能修正
    Signed-off-by: AlexFanw <alex8802732@gmail.com>

--- Toefl.js
+++ Toefl.js
@@ -1,8 +1,8 @@
 //在这里加入你需要选择的城市。
-var city_choose = ["武汉","长沙","南昌"]
+var city_choose = ["武汉","长沙","郑州"]
 
 //在这里加入你需要选择的时间[start_time,end_time]。
-var time_start_end = ["2021-4-10","2021-10-30"]
+var time_start_end = ["2021-4-2","2021-10-1"]
 
 //搜索全部时间
 //var time_start_end = ["2000-4-10","2099-10-30"]
@@ -21,6 +21,7 @@ async function Scanner(city_choose, all) {
     //循环查找
     for (i = 1; i < city.options.length; ++i) {
         for(n = 0; n < city_choose.length; n++){
+            if(n==1&&all==true){break} //在全局搜索模式下，不再匹配city_choose里面的城市
             if(city_choose[n]==city.options[i].text||all==true){ //如果找到了指定的城市
                 city.options[i].selected = true
                 for (j = 1; j < day.options.length; ++j) {
@@ -39,7 +40,7 @@ async function Scanner(city_choose, all) {
                                         tb.rows[row].cells[1].innerText)
 }}}}}}}}}
 
-
+// 日期时间比对函数
 function compare(date){
     
     var cur = new Date(date.slice(0,date.indexOf("日")).replace("年","-").replace("月","-"));
